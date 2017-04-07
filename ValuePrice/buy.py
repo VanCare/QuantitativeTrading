cae7cf9c7f26ef66666cdc24affc415c22f2eb32
VanCare <VanCare1217@gmail.com>
2017-04-07 21:13:02 +0800

    repo initialization

---
+++ ValuePrice/buy.py
@@ -0,0 +1,6 @@
+from func import *
+import numpy as np
+import time
+
+#数据结构为(time,price)
+dataType=np.dtype({'name':['time','price'],'format':[]})
---
+++ ValuePrice/func.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+def filter_time(,)
+
+def value
